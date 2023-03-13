4e76e7ad3db204e25da717b8a6fa59afa4fba71d
Leone Mendes <leonemendes23@gmail.com>
2023-03-12 22:38:01 -0300

    First commit: initial files added.

---
+++ set_path.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set "PYTHONDIR=C:\Users\Leone\AppData\Local\Programs\Python\Python310"
+
+
+set "PATH=%PYTHONDIR%;%PYTHONDIR%/Scripts;%PATH%"
+set "PYTHONPATH=%PYTHONDIR%\Lib;%PYTHONDIR%\Lib\site-packages;%PYTHONDIR%\DLLs;"
---
+++ simple_model.py
@@ -0,0 +1,57 @@
+'''
+This code defines the vehicle parameters, initial conditions, and simulation parameters at the beginning. 
+It then defines the net force function, which returns the net force on the vehicle as a function of time. 
+In this example, the net force is a constant 1000 N.
+
+The code initializes arrays to store the position and velocity of the vehicle at each time step, and then 
+loops over each time step to update the position and velocity using the Euler integration method.
+
+Finally, the code plots the position of the vehicle over time using the Matplotlib library.
+
+Note that this is a very basic example of 1D vehicle dynamics simulation, and you may need to modify the code 
+to include more realistic vehicle parameters and net force functions.
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define vehicle parameters
+m = 1000.0  # mass in kg
+
+# Define initial conditions
+x0 = 0.0  # initial position in meters
+v0 = 10.0  # initial velocity in m/s
+
+# Define simulation parameters
+dt = 0.01  # time step in seconds
+T = 10.0  # total simulation time in seconds
+
+# Define net force function
+def net_force(t):
+    # Example function that returns a constant force of 1000 N
+    return 1000.0
+
+# Initialize arrays to store simulation results
+t_array = np.arange(0, T, dt)
+x_array = np.zeros_like(t_array)
+v_array = np.zeros_like(t_array)
+
+# Set initial conditions
+x_array[0] = x0
+v_array[0] = v0
+
+# Loop over time steps
+for i in range(1, len(t_array)):
+    # Compute acceleration using equation of motion
+    a = net_force(t_array[i]) / m
+    
+    # Update velocity and position using Euler integration
+    v_array[i] = v_array[i-1] + a * dt
+    x_array[i] = x_array[i-1] + v_array[i] * dt
+
+# Plot results
+
+plt.plot(t_array, x_array)
+plt.xlabel('Time (s)')
+plt.ylabel('Position (m)')
+plt.show()
